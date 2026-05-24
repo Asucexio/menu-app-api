ae6828ba7841088ec1f15af7a689c09b5dec14e4
Asmamew Ademasu <nsr.122.15@amu.edu.et>
2026-05-24 11:22:31 +0300

    orgin problem ficed

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ const app = express();
 
 // ── CORS — allow all origins in dev, specific in prod ────────
 const allowedOrigins = process.env.CLIENT_URL
-  ? [process.env.CLIENT_URL, 'http://localhost:3000', 'http://127.0.0.1:3000']
-  : ['http://localhost:3000', 'http://127.0.0.1:3000'];
+  ? [process.env.CLIENT_URL, 'https://qr-menu-app-snowy.vercel.app', 'http://127.0.0.1:3000']
+  : [, 'https://qr-menu-app-snowy.vercel.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
